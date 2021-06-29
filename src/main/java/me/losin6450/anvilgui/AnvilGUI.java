c80d2febf85ef277582d4ec1678c265435412ee8
Losin6450 <84465156+Losin6450@users.noreply.github.com>
2021-06-29 21:14:28 +0200

    Add files via upload

---
+++ src/main/java/me/losin6450/anvilgui/AnvilGUI.java
@@ -0,0 +1,20 @@
+package me.losin6450.anvilgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+import net.wesjd.anvilgui.AnvilGUI.Builder;
+
+public final class AnvilGUI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
